a12385863415277a9ba36a923e545317f74da8fc
bigstar9906 <bigstar9906@naver.com>
2024-06-24 09:55:20 +0900

    [test] Expo 환경 세팅

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
+import * as IntentLauncher from 'expo-intent-launcher';
+import * as Sharing from 'expo-sharing';
+
+const InstagramShare = async () => {
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.All,
+    quality: 1,
+  });
+  console.log(result);
+
+  if (!result.cancelled) {
+    const imageUri = await FileSystem.getContentUriAsync(result.assets[0].uri);
+    console.log(imageUri);
+      IntentLauncher.startActivityAsync('com.instagram.share.ADD_TO_STORY', {
+        data: imageUri,
+        flags: 1, // FLAG_GRANT_READ_URI_PERMISSION
+        type: 'image/jpeg', // or other based on your file type
+      }).catch(console.warn);
+  }
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>123124</Text>
+      <Button title="test" onPress={InstagramShare}/>
       <StatusBar style="auto" />
     </View>
   );
