8881c4dff3b839ded5e797e3483b590cfd6e32ca
yunred <nado0903@gmail.com>
2024-06-24 13:00:32 +0900

    Feat: Tailwind 추가

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import * as FileSystem from 'expo-file-system';
-import * as ImagePicker from 'expo-image-picker';
-import * as IntentLauncher from 'expo-intent-launcher';
-import * as Sharing from 'expo-sharing';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+import * as IntentLauncher from "expo-intent-launcher";
+import * as Sharing from "expo-sharing";
 
 const InstagramShare = async () => {
   let result = await ImagePicker.launchImageLibraryAsync({
@@ -15,29 +15,34 @@ const InstagramShare = async () => {
   if (!result.cancelled) {
     const imageUri = await FileSystem.getContentUriAsync(result.assets[0].uri);
     console.log(imageUri);
-      IntentLauncher.startActivityAsync('com.instagram.share.ADD_TO_STORY', {
-        data: imageUri,
-        flags: 1, // FLAG_GRANT_READ_URI_PERMISSION
-        type: 'image/jpeg', // or other based on your file type
-      }).catch(console.warn);
+    IntentLauncher.startActivityAsync("com.instagram.share.ADD_TO_STORY", {
+      data: imageUri,
+      flags: 1, // FLAG_GRANT_READ_URI_PERMISSION
+      type: "image/jpeg", // or other based on your file type
+    }).catch(console.warn);
   }
 };
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 bg-green-500 flex items-center justify-center">
       <Text>123124</Text>
-      <Button title="test" onPress={InstagramShare}/>
+      <Button title="test" onPress={InstagramShare} />
       <StatusBar style="auto" />
+      <View>
+        <Text className="text-3xl">
+          Open up App.js to start working on your app!
+        </Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
