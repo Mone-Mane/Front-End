8b5413a5c3e8cb6155d6281196ab5ccba0aebd7d
Wonhee <totohwh3601@naver.com>
2024-06-25 14:39:39 +0900

    [feat] status bar 기능 구현

--- screens/Test.jsx
+++ screens/Test.jsx
@@ -1,15 +1,30 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import AccountHistory from "../components/AccountHistory";
+import ProgressBar from "../components/ProgressBar";
+import { useState } from 'react';
+
 
 const Test = () => {
+  const [progress, setProgress] = useState(70);
   return (
     <View>
       <AccountHistory/>
+      <View style={styles.appContainer}>
+        <ProgressBar progress={progress} />
+      </View>
     </View>
+    
   );
 };
 
 export default Test;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+});
