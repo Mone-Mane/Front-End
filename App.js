dc72d9639e4ba1f8c12f23b3a614eb7e96bb1452
yunred <nado0903@gmail.com>
2024-06-25 16:59:06 +0900

    Feat: Customheader 추가

--- App.js
+++ App.js
@@ -5,22 +5,27 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
 import Test from "./screens/Test";
-import PhoneBook from "./screens/PhoneBook";
+import PhoneBook from "./screens/PhoneBook.jsx";
 import DiaryHome from "./screens/picturediary/DiaryHome.jsx";
 import AccountHistory from "./components/AccountHistory.jsx";
+import SYTest from "./screens/SYTest.jsx";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     // <View>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="Test" component={Test} />
         <Stack.Screen name="PhoneBook" component={PhoneBook} />
         <Stack.Screen name="그림일기" component={DiaryHome} />
-        <Stack.Screen name="계좌내역" component={AccountHistory}/>
+        <Stack.Screen name="계좌내역" component={AccountHistory} />
+        <Stack.Screen name="SYTest" component={SYTest} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
